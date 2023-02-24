247b2c5c25ce2a3bd8053db261a2eae1e4f94d63
Sebastian Stec <sebastianstec@Sebastians-MacBook-Pro.local>
2023-02-24 13:00:14 -0500

    moved locations to separate time slots

--- Community-Board/src/components/Calendar.jsx
+++ Community-Board/src/components/Calendar.jsx
@@ -22,7 +22,7 @@ const Calendar = () => {
                     <td className="time">8 am</td>
                     <Event color='blue'></Event>
                     <td></td>
-                    <Event event='Offense Training' color='yellow' location='Home Field'></Event>
+                    <Event event='Offense Training' color='yellow'></Event>
                     <Event color='blue'></Event>
                     <td></td>
                     <td></td>
@@ -32,30 +32,30 @@ const Calendar = () => {
                     <td className="time">9 am</td>
                     <Event event='B' color='blue'></Event>
                     <td></td>
-                    <Event color='yellow'></Event>
+                    <Event color='yellow' location='Home Field'></Event>
                     <Event event='B' color='blue'></Event>
-                    <Event event='Team Tactics' color='purple' location='Study Room'></Event>
+                    <Event event='Team Tactics' color='purple'></Event>
                     <td></td>
                     <Event event='MATCHDAY' color='orange'></Event>
                 </tr>
                 <tr>
                     <td className="time">10 am</td>
                     <Event color='blue'></Event>
-                    <Event event='Defense Training' color='green' location='Home Field'></Event>
+                    <Event event='Defense Training' color='green'></Event>
                     <Event color='yellow'></Event>
                     <Event color='blue'></Event>
-                    <Event color='purple'></Event>
+                    <Event color='purple' location='Study Room'></Event>
                     <td></td>
                     <Event event='vs. Catalonia' color='orange' location='Away'></Event>
                 </tr>
                 <tr>
                     <td className="time">11 am</td>
                     <Event event='R' color='blue'></Event>
-                    <Event color='green'></Event>
+                    <Event color='green' location='Home Field'></Event>
                     <td></td>
                     <Event event='R' color='blue'></Event>
                     <Event color='purple'></Event>
-                    <Event event='General Warm-Up' color='red' location='Home Field'></Event>
+                    <Event event='General Warm-Up' color='red'></Event>
                     <Event event='Kickoff at 11am' color='orange'></Event>
                 </tr>
                 <tr>
@@ -65,7 +65,7 @@ const Calendar = () => {
                     <td></td>
                     <Event color='blue'></Event>
                     <Event color='purple'></Event>
-                    <Event color='red'></Event>
+                    <Event color='red' location='Home Field'></Event>
                     <Event color='orange'></Event>
                 </tr>
                 <tr>
@@ -91,21 +91,21 @@ const Calendar = () => {
                 <tr>
                     <td className="time">3 pm</td>
                     <Event event='A' color='blue'></Event>
-                    <Event event='Tape Review' color='pink' location='Theatre'></Event>
-                    <Event event='Tape Review' color='pink' location='Theatre'></Event>
+                    <Event event='Tape Review' color='pink'></Event>
+                    <Event event='Tape Review' color='pink'></Event>
                     <Event event='A' color='blue'></Event>
-                    <Event event='Tape Review' color='pink' location='Theatre'></Event>
-                    <Event event='Tape Review' color='pink' location='Theatre'></Event>
+                    <Event event='Tape Review' color='pink'></Event>
+                    <Event event='Tape Review' color='pink'></Event>
                     <td></td>
                 </tr>
                 <tr>
                     <td className="time">4 pm</td>
                     <Event color='blue'></Event>
-                    <Event color='pink'></Event>
-                    <Event color='pink'></Event>
+                    <Event color='pink' location='Theatre'></Event>
+                    <Event color='pink' location='Theatre'></Event>
                     <Event color='blue'></Event>
-                    <Event color='pink'></Event>
-                    <Event color='pink'></Event>
+                    <Event color='pink' location='Theatre'></Event>
+                    <Event color='pink' location='Theatre'></Event>
                     <td></td>
                 </tr>
                 <tr>
